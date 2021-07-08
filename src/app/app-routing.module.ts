1ff447f59cfaf7dc5ed3e2c0bddf3a1c073efa62
AngelPezcadena <xxhercoxx@gmail.com>
2021-07-08 11:44:22 -0500

    colores buap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeStudentComponent } from './pages/student/home-student/home-student.component';
 import { HomeProfessorComponent } from './pages/professor/home-professor/home-professor.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'login', component:LoginComponent
+  },
   {
     path: 'homeStudent', component:HomeStudentComponent
   },
