7408411e17e787291bab9a0ce3faf7637b278079
Angel Pezcadena <xxhercoxx@gmail.com>
2021-09-08 14:35:30 -0500

    examenes de contenido ya guardan intentos y apro

---
+++ src/app/interfaces/test-habilidades.ts
@@ -0,0 +1,10 @@
+export interface TestHabilidades {
+    fecha_test: Date;
+    p1_esquema: any[];
+    p2_lectura: any[];
+    p3_video: any[];
+    p4_esquema: any[];
+    p5_lectura: any[];
+    p6_video: any[];
+    contenido: string;
+}
--- src/app/pages/student/subject-student/subject-student.component.ts
+++ src/app/pages/student/subject-student/subject-student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { SubjectsService } from 'src/app/services/subjects.service';
 
 @Component({
@@ -12,10 +13,22 @@ export class SubjectStudentComponent implements OnInit {
   subject:any;
   progress:number=0;
   partName:string="Lección";
+  sesion:any;
 
-  constructor( private activatedRoute: ActivatedRoute, private subjects: SubjectsService ) { }
+  constructor( private activatedRoute: ActivatedRoute, private subjects: SubjectsService, private auth:AuthService ) { }
 
   ngOnInit(): void {
+    this.getParams();
+    this.getSesion();
+  }
+
+  getSesion(){
+    this.auth.obtenerSesion().then(res=>{
+      this.sesion = res;
+    });
+  }
+
+  getParams(){
     this.activatedRoute.params.subscribe(params=>{
       console.log("Subject",params.subject);
       this.subject = this.subjects.getSubject(params.subject);
