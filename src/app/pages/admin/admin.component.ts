62734ab2113e8cb2369ef2af53112fde846238af
Angel Pezcadena <xxhercoxx@gmail.com>
2021-10-05 14:00:11 -0500

    Bloqueado admin con contraseña suave 123

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin',
@@ -9,18 +11,47 @@ export class AdminComponent implements OnInit {
 
   select:string = "0";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     var res = localStorage.getItem("admin");
     if(res){
       this.select = res;
     }
+    this.logAdmin();
   }
 
   selectB(select:string){
     this.select = select;
     localStorage.setItem("admin",select);
+  }
+
+  async logAdmin(){
+    const value  = await Swal.fire({
+      position: 'center',
+      title: 'Ingresa contraseña:',
+      input: 'password',
+      inputLabel: 'Contraseña',
+      inputPlaceholder: 'Ingresa contraseña',
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      showCancelButton : true
+    });
+    
+    if (value.isDismissed) {
+      this.router.navigate(['/']);
+    } else {
+      if (value.value != "123") {
+        await Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Verifica los datos proporcionados',
+          showConfirmButton: false,
+          timer: 3000
+        });
+        this.logAdmin();
+      }
+    }
   }
 
 }
