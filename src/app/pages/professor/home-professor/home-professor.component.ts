6546f87bdc25055accf3d947d848f7a4d45b35dc
Angel Pezcadena <xxhercoxx@gmail.com>
2021-10-11 14:02:46 -0500

    profesor notificaciones y cambiar contraseña

--- src/app/pages/professor/home-professor/home-professor.component.ts
+++ src/app/pages/professor/home-professor/home-professor.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Notification } from 'src/app/interfaces/notification';
 import { Subject } from 'src/app/interfaces/subject';
+import { Aviso, Usuario } from 'src/app/interfaces/usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 import { Group } from '../../../interfaces/group';
 
 @Component({
@@ -15,14 +20,55 @@ export class HomeProfessorComponent implements OnInit {
     {name:"Matodologia de la programación-2021-2", students:32,contentComplete:14,contentTotal:56}
   ]
 
-  notifications : Notification[] = [
-    {message:"Bienvenido a SIMA, te invitamos a explorar tus materias",active:true},
-    {message:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo voluptatum",active:false}
-  ]
+  notifications : Notification[] = [];
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private _authService: AuthService,
+    private router: Router,
+    private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.createNewForm();
+    this._authService.obtenerDatosBasicosUsuario().then(  ( res:Usuario ) => {
+      //*Cargar avisos
+      res.avisos?.forEach( (obj:Aviso) => {
+        let nuevoObj:Notification = { message: obj.mensaje, active: obj.visto }
+        this.notifications.push(nuevoObj);
+      })
+    })
+  }
+
+  createNewForm(){
+    this.form = this.fb.group({
+      password1: ["", [ Validators.required, Validators.minLength(8) ] ],
+      password2: ["", [ Validators.required, Validators.minLength(8) ] ],
+    });
+  }
+
+  submitForm(){
+    
+    if( this.form.controls["password1"].value != this.form.controls["password2"].value ){
+      this.form.setErrors({invalid:true});
+    }
+
+    if( this.form.invalid ){
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Las contraseñas deben coincidir y deben tener minimo 8 caracteres.',
+        showConfirmButton: true,
+        confirmButtonColor: '#3085d6',
+        timer: 5000
+      });
+      this.form.markAllAsTouched();
+      
+    }else{
+    //LOG:
+    console.log( this.form );
+    this._authService.setNuevoPassword( this.form.controls["password1"].value )
+    //!Enviar nueva contraseña
+    }
   }
 
 }
