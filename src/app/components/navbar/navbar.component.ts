c3c15522d724c25467a80f30eff8216cbde432b3
Angel Pezcadena <xxhercoxx@gmail.com>
2021-10-20 15:56:30 -0500

    navbar dirige a donde debe

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent implements OnInit {
   displayName: string | null = "";
   photo: string | null = "";
   rolName: string | null = "";
+  role:any;
 
   constructor(  private _authService: AuthService,
                 private router: Router ) {
@@ -27,8 +28,10 @@ export class NavbarComponent implements OnInit {
   getRolName( rolName:string | null ){
     switch (rolName) {
       case 'alumnos':
+        this.role = '/homeStudent'
         return 'Estudiante'
       default:
+        this.role = '/homeProfessor'
         return 'Docente';
     }
   }
